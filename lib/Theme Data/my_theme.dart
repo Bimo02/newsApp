e17487ca85b27fd4822830e2afbdcb8d30f5d1b4
Ali Ramadan <alir00330@gmail.com>
2023-05-13 14:10:35 +0300

    first prototyp of the app

---
+++ lib/Theme Data/my_theme.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+class MyThemeData {
+  static Color lightprim = Color(0xFFDFECDB);
+  static Color darkprim = Color(0xFF060E1E);
+  static Color colorblack = Color(0xFF242424);
+  static Color colorwihte = Color(0xFFFFFFFF);
+  static Color lightprimlow = Color(0xFF5D9CEC);
+  static Color textsec = Color(0xFF61E757);
+  static Color textprim = Color(0xFF5D9CEC);
+  static Color darksec = Color(0xFF5D9CEC);
+  static Color color_red = Color(0xFFEC4B4B);
+  static Color blacklight = Color(0xFF141922);
+  static ThemeData lightTheme = ThemeData(
+      primaryColor: lightprim,
+      primaryColorLight: lightprimlow,
+      backgroundColor: colorwihte,
+      dividerColor: lightprimlow,
+      hoverColor: lightprimlow,
+      unselectedWidgetColor: Colors.white,
+      highlightColor: colorwihte,
+      iconTheme: IconThemeData(color: lightprim),
+      iconButtonTheme: IconButtonThemeData(
+          style: ButtonStyle(iconColor: MaterialStateProperty.all(lightprim))),
+      scaffoldBackgroundColor: lightprim, //Scaffold
+      appBarTheme: AppBarTheme(
+          elevation: 0,
+          backgroundColor: lightprimlow,
+          iconTheme: IconThemeData(color: colorblack)),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          selectedItemColor: lightprimlow,
+          unselectedItemColor: Color(0xFFC8C9CB)),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: lightprimlow,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50),
+              side: BorderSide(color: Colors.white, width: 4))),
+      textTheme: TextTheme(
+        headline1: GoogleFonts.poppins(
+            color: MyThemeData.colorwihte,
+            fontWeight: FontWeight.bold,
+            fontSize: 22),
+        headline2: GoogleFonts.poppins(
+            color: MyThemeData.colorblack,
+            fontWeight: FontWeight.bold,
+            fontSize: 14),
+        subtitle1: GoogleFonts.roboto(
+            color: Color(0XFF383838),
+            fontWeight: FontWeight.bold,
+            fontSize: 18),
+        subtitle2: GoogleFonts.roboto(
+            color: Color(0XFFA9A9A9),
+            fontWeight: FontWeight.bold,
+            fontSize: 20),
+        labelMedium: GoogleFonts.roboto(
+            color: lightprimlow, fontWeight: FontWeight.bold, fontSize: 14),
+        headline3: GoogleFonts.poppins(
+            color: MyThemeData.lightprimlow,
+            fontWeight: FontWeight.bold,
+            fontSize: 18),
+        headline4: GoogleFonts.roboto(
+            color: MyThemeData.blacklight,
+            fontWeight: FontWeight.bold,
+            fontSize: 15),
+        headline5: GoogleFonts.roboto(
+            color: MyThemeData.lightprimlow,
+            fontWeight: FontWeight.bold,
+            fontSize: 15),
+        headline6: GoogleFonts.roboto(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 17),
+      ));
+  static ThemeData darktheme = ThemeData(
+      primaryColor: darkprim,
+      primaryColorLight: darksec,
+      hoverColor: darksec,
+      backgroundColor: darksec,
+      dividerColor: darksec,
+      highlightColor: blacklight,
+      iconTheme: IconThemeData(color: darkprim),
+      iconButtonTheme: IconButtonThemeData(
+          style: ButtonStyle(
+              iconColor: MaterialStateProperty.all(darkprim),
+              backgroundColor: MaterialStateProperty.all(darkprim))),
+      scaffoldBackgroundColor: darkprim,
+      appBarTheme: AppBarTheme(
+        elevation: 0,
+          backgroundColor: darksec,
+          iconTheme: IconThemeData(color: colorblack)),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        elevation: 0,
+          backgroundColor: blacklight,
+          selectedItemColor: darksec,
+          unselectedItemColor: colorwihte),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        elevation: 0,
+          backgroundColor: darksec,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50),
+              side: BorderSide(color: blacklight, width: 4))),
+      textTheme: TextTheme(
+        headline1: GoogleFonts.poppins(
+            color: MyThemeData.blacklight,
+            fontWeight: FontWeight.bold,
+            fontSize: 22),
+        headline2: GoogleFonts.poppins(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 14),
+        subtitle1: GoogleFonts.roboto(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 18),
+        subtitle2: GoogleFonts.roboto(
+            color:  Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20),
+        labelMedium: GoogleFonts.roboto(
+            color: lightprimlow, fontWeight: FontWeight.bold, fontSize: 14),
+        headline3: GoogleFonts.poppins(
+            color: MyThemeData.lightprimlow,
+            fontWeight: FontWeight.bold,
+            fontSize: 18),
+        headline4: GoogleFonts.roboto(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 15),
+        headline5: GoogleFonts.roboto(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 15),
+        headline6: GoogleFonts.roboto(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 17),
+      ));
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/providers/app_config_provider.dart';
+import 'package:newsapp/providers/themeProvider.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'Home/homeScreen.dart';
+import 'Theme Data/my_theme.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
+//  FirebaseFirestore.instance.settings =
+  //    Settings(cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED);
+  //await FirebaseFirestore.instance.disableNetwork();
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  final isDark =sharedPreferences.getBool('is_dark') ?? false;
+  final isLanguage = sharedPreferences.getString('Ar')??'';
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (context)=> AppConfigProvider(isLanguage)),
+    ChangeNotifierProvider(create: (context) => ThemeProvider(isDark)),
+  ], child: MyApp(isDark: isDark, isLanguage: isLanguage,)));
+}
+
+class MyApp extends StatelessWidget {
+  final bool isDark;
+  final String isLanguage;
+  MyApp({required this.isDark , required this.isLanguage});
+  @override
+  Widget build(BuildContext context) {
+    var provider = Provider.of<AppConfigProvider>(context);
+    var providerTH = Provider.of<ThemeProvider>(context);
+    return MaterialApp(
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+      initialRoute: HomeScreen.routeName,
+      theme: MyThemeData.lightTheme,
+      darkTheme: MyThemeData.darktheme,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      locale: Locale(provider.appLanguage),
+      themeMode: providerTH.apptheme,
+    );
+  }
+}
+
